4e4a7646d9ea6bff6ea25a8d7ef1d5e72ea6d1c3
Joel James <jayfiled@gmail.com>
2019-06-07 23:39:15 +1000

    made static version of ticket display, added event listeners and animations

--- public/application.js
+++ public/application.js
@@ -1,7 +1,11 @@
-const ticketBody = document.querySelector('.ticket-body');
+let idIncrementer = 0
+const ticketTitle = document.querySelectorAll('.ticket-title');
 
 addRevealClass = (event) => {
-  ticketBody.classList.toggle('relative-opacity');
+  event.currentTarget.parentElement.children[1].classList.toggle('relative-opacity');
 }
 
-ticketBody.addEventListener('click', addRevealClass);
+ticketTitle.forEach((element) => {
+  element.addEventListener('click', addRevealClass);
+  idIncrementer += 1;
+});
