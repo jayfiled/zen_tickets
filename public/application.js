b778b19c917f7298bf5d7c33a2567ccb12375b49
Joel James <jayfiled@gmail.com>
2019-06-09 12:02:30 +1000

    add buttons to see more tickets, event listeners, tweak css

--- public/application.js
+++ public/application.js
@@ -1,17 +1,32 @@
-// console.log("JS working")
+
+// Delay adding of click event handlers until text has faded in
 
 const delayedFunc = () => {
-  console.log("Running")
-  const ticketWrapper = document.querySelectorAll('.ticket-wrapper');
 
+  // Target the elements that need a click event
+  const getStartedBtn = document.getElementById('get-started')
+  const ticketWrappers = document.querySelectorAll('.ticket-wrapper');
+  const moreBtns = document.querySelectorAll('.more-button')
+
+  // Callbacks to toggle classes
+  const showFirstTickets = (event) => {
+    event.currentTarget.parentElement.parentElement.nextElementSibling.classList.toggle('in-left');
+  }
   const addRevealClass = (event) => {
     event.currentTarget.children[0].children[1].classList.toggle('relative-opacity');
   }
+  const seeMoreTickets = (event) => {
+    console.log('clicked')
+    event.currentTarget.parentElement.nextElementSibling.classList.toggle('in-left');
+  }
 
-  ticketWrapper.forEach((element) => {
-    console.log('added')
-    element.addEventListener('click', addRevealClass);
-  });
+
+  // Event listeners
+  getStartedBtn.addEventListener('click', showFirstTickets);
+  ticketWrappers.forEach(el => el.addEventListener('click', addRevealClass));
+  moreBtns.forEach(el => el.addEventListener('click', seeMoreTickets));
+
+
 }
 
 setTimeout(delayedFunc, 1100);
